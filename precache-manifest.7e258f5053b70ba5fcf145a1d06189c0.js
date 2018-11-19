18c1a4e263d03aa4b9e033ee05f59b82807132c5
Fukuwa <fjoriofi@gmail.com>
2018-11-19 03:58:33 +0200

    Added app f iles

---
+++ precache-manifest.7e258f5053b70ba5fcf145a1d06189c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-react/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a503e9b9cb4043d883fb",
+    "url": "/robo-react/static/js/runtime~main.a503e9b9.js"
+  },
+  {
+    "revision": "bd4cf03823b5193470a3",
+    "url": "/robo-react/static/js/main.bd4cf038.chunk.js"
+  },
+  {
+    "revision": "3b0bd539e3231e2d05c6",
+    "url": "/robo-react/static/js/1.3b0bd539.chunk.js"
+  },
+  {
+    "revision": "bd4cf03823b5193470a3",
+    "url": "/robo-react/static/css/main.4afe4429.chunk.css"
+  },
+  {
+    "revision": "3b0bd539e3231e2d05c6",
+    "url": "/robo-react/static/css/1.8adae16e.chunk.css"
+  },
+  {
+    "revision": "7c4ef5d56f8eeb94a8a228e755d20134",
+    "url": "/robo-react/index.html"
+  }
+];
